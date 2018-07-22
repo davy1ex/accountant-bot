622483a2956412a565f452f82f9e1674ae79c472
davy1ex <32157455+davy1ex@users.noreply.github.com>
2018-07-22 12:49:59 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import os
 
 from vk_api import *
 from vk_api.longpoll import *
+from auth import LOGIN, PASSWORD
 
 print("Accountant-bot by davy1ex")
 db_name = 'db.txt'
@@ -14,24 +15,25 @@ def first_start():
     if 'users_invite.txt' not in os.listdir():
         with open('users_invite.txt', 'w') as f:
             f.write('Добавились в беседу:\n')
-
-
-def get_me(vk):
-    """ получает имя авторизовавшегося пользователя"""
-    info = vk.users.get(fields="nickname")
-    return info
+    if db_name not in os.listdir():
+        with open(db_name, "w") as f:
+            f.write("")
 
 
 def get_name_this_user(id=None):
     info = vk.users.get(fields="nickname", user_ids=id)
     first_name = info[0]["first_name"]
     last_name = info[0]["last_name"]
-    name = first_name + " " + last_name
 
-    return name
+    return first_name + " " + last_name
 
 
 def get_text_from_file(file):
+    """
+    возвращает текст из файла
+    :param file: откуда нужно вернуть текст
+    :return:
+    """
     with open(file, 'r') as f:
         return f.read()
 
@@ -45,10 +47,8 @@ def delete_user(file, text):
     # пример комманды: /удалить Вася Пупкин
     with open(file, "r") as f:
         text = text.split("/удалить ")[1]
-        print("TEXT:", text)
         new_f_text = ""
         for line in f.readlines():
-            print("LINE: " + line + "\n")
             if text not in line:
                 new_f_text += line
     with open(file, "w") as f:
@@ -77,6 +77,7 @@ def clear_all_users(file, first_words=""):
     """
     Очищает бд
     :param file: файл бд
+    :param first_words: слова, которые надо вставить вначало файла
     """
     with open(file, "w") as file:
         file.write(first_words)
@@ -86,32 +87,28 @@ def clear_all_users(file, first_words=""):
 first_start()
 
 # авторизация
-while True:
-    from auth import LOGIN, PASSWORD
+try:
+    v = VkApi(login=LOGIN, password=PASSWORD)
+    v.auth()
+    lp = VkLongPoll(v)
+    vk = v.get_api()
+except Captcha:
+    print("Лимит попыток авторизации исчерпан, заходи позже")
+    exit()
 
-    try:
-        v = VkApi(login=LOGIN, password=PASSWORD)
-        v.auth()
-        break
-    except Captcha:
-        print("Лимит попыток авторизации исчерпан, заходи позже")
-        exit()
 
-lp = VkLongPoll(v)
-vk = v.get_api()
-
-first_name = get_me(vk)[0]["first_name"]
-last_name = get_me(vk)[0]["last_name"]
-name = first_name + " " + last_name
+name = get_name_this_user()
 
 print("{0}: Авторизация прошла успешно".format(name))
+print("Чтобы начать работу с ботом:\n"
+      "\t1.Закиньте его в беседу;\n"
+      "\t2. Напишите \"/помощь\";\n"
+      "\t3. Наслаждайтесь.")
+
 
 while True:
     for event in lp.listen():
         if event.type == VkEventType.MESSAGE_NEW and 'source_act' in event.raw[7]:
-            print(event.type, event.raw)
-            print(event.type, event.raw[7])
-            ## print(vk.notifications.get())
             name = get_name_this_user(event.raw[7]['source_mid'])
             if event.raw[7]['source_act'] == 'chat_kick_user' and \
                     name not in get_text_from_file('users_kick.txt'):
@@ -128,7 +125,7 @@ while True:
                 add_new_user(file=db_name, text=text)
                 vk.messages.send(
                     chat_id=event.chat_id,
-                    message="Готово",
+                    message="Готово.",
                     forward_messages=event.message_id
                 )
 
@@ -137,13 +134,17 @@ while True:
                     chat_id=event.chat_id,
                     message="Команды:"
                             "\n/добавить Вася Пупкин - добавляет Вася Пупкина в базу данных (дальше бд)\n"
-                            "/покажи - показывает бд\n"
-                            "/очистить - очищает бд\n"
-                            "/удалить Вася Пупкин - удаляет Васю Пупкина из бд",
+                            "/покажи бд - показывает бд\n"
+                            "/очистить бд - очищает бд\n"
+                            "/удалить Вася Пупкин - удаляет Васю Пупкина из бд\n"
+                            "/очисть кто ушёл - очищает списки тех, кто ушёл\n"
+                            "/очистить кто пришёл - очищает списки тех, кто пришёл\n"
+                            "/кто ушёл - показывает тех, кто ушёл\n"
+                            "/кто пришёл - показывает тех, кто пришёл",
                     forward_messages=event.message_id
                 )
 
-            elif "покажи" in text.lower():
+            elif "покажи бд" in text.lower():
                 with open(db_name, "r") as file:
                     vk.messages.send(
                         chat_id=event.chat_id,
@@ -151,9 +152,25 @@ while True:
                         forward_messages=event.message_id
                     )
 
-            elif "очистить" in text.lower():
+            elif "очистить кто ушёл" in text.lower():
+                clear_all_users(first_words='Беседу покинули:\n', file="users_kick.txt")
+                vk.messages.send(
+                    chat_id=event.chat_id,
+                    message="Готово.",
+                    forward_messages=event.message_id
+                )
+
+            elif "очистить кто пришёл" in text.lower():
+                clear_all_users(first_words='Добавились в беседу:\n', file="users_invite.txt")
+                vk.messages.send(
+                    chat_id=event.chat_id,
+                    message="Готово.",
+                    forward_messages=event.message_id
+                )
+
+            elif "очистить бд" in text.lower():
                 clear_all_users(db_name)
-                message = "Готово"
+                message = "Готово."
                 vk.messages.send(
                     chat_id=event.chat_id,
                     message=message,
@@ -161,12 +178,19 @@ while True:
                 )
 
             elif "удалить" in text.lower():
-                delete_user(db_name, text)
-                vk.messages.send(
-                    chat_id=event.chat_id,
-                    message="Готово",
-                    forward_messages=event.message_id
-                )
+                if len(text.lower().split()) > 1:
+                    delete_user(db_name, text)
+                    vk.messages.send(
+                        chat_id=event.chat_id,
+                        message="Готово.",
+                        forward_messages=event.message_id
+                    )
+                else:
+                    vk.messages.send(
+                        chat_id=event.chat_id,
+                        message="А удалить то мне кого?",
+                        forward_messages=event.message_id
+                    )
 
             elif "кто ушёл" in text.lower():
                 vk.messages.send(
@@ -180,23 +204,4 @@ while True:
                     chat_id=event.chat_id,
                     message=get_text_from_file('users_invite.txt'),
                     forward_messages=event.message_id
-                )
-
-            elif "очистить кто ушёл" in text.lower():
-                clear_all_users(first_words='Беседу покинули:\n', file="users_kick.txt")
-                vk.messages.send(
-                    chat_id=event.chat_id,
-                    message="Готово.",
-                    forward_message=event.message_id
-                )
-
-            elif "очистить кто пришёл" in text.lower():
-                with open("users_invite.txt", "w") as f:
-                    f.write("")
-                    # f.write("Добавились в беседу:\n")
-                # clear_all_users(first_words='Добавились в беседу:\n', file="users_invite.txt")
-                vk.messages.send(
-                    chat_id=event.chat_id,
-                    message="Готово.",
-                    forward_message=event.message_id
                 )
