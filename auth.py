4d8bf68a683899a28f4df3e4ce73c690b847fa6d
davy1ex <32157455+davy1ex@users.noreply.github.com>
2018-07-22 12:51:09 +0400

    small updates

--- auth.py
+++ auth.py
@@ -12,3 +12,19 @@ while True:
         break
     except BadPassword:
         print("Неверный логин или пароль")
+
+
+# from getpass import getpass
+# from vk_api import *
+#
+# while True:
+#     LOGIN = str(input("Логин: "))
+#     PASSWORD = str(getpass("Пароль: "))
+#     try:
+#         v = VkApi(login=LOGIN, password=PASSWORD)
+#         v.auth()
+#         with open("auth.py", 'w') as f:
+#             f.write("LOGIN = '{0}', PASSWORD = '{1}'".format(LOGIN, PASSWORD)
+#         break
+#     except BadPassword:
+#         print("Неверный логин или пароль")
